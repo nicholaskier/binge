277344a46ccde4e23290a3e2ab6c6c763aea2370
nicholaskier <nicholaskier@gmail.com>
2021-01-14 13:24:52 -0600

    update

---
+++ src/pages/TVShowList/TVShowList.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import './TVShowList.css'
+import * as tvshowAPI from '../../services/tvshows-api'
+import TVShowCard from '../../components/TVShowCard/TVShowCard'
+
+function TVShowList(props) {
+
+    const [tvshows, setTvshows] = useState([])
+
+    async function handleDeleteTVShow(id){
+        await tvshowAPI.deleteOne(id)
+        setTvshows(tvshows.filter(t => t._id !== id))
+    }
+
+    useEffect( () => {
+        (async function(){
+            const tvshows = await tvshowAPI.getAll()
+            setTvshows(tvshows)
+        })()
+    }, [])
+
+    return (
+        <>
+            <div className='TVShowList-grid'>
+                {tvshows.map(tvshow =>
+                    <TVShowCard 
+                        key={tvshow._id}
+                        tvshow={tvshow}
+                        user={props.user}
+                        handleDeleteTVShow={handleDeleteTVShow}
+                    />
+                )}
+            </div>
+    </>
+    )
+}
+
+export default TVShowList
